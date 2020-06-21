c4baa2a749a222b19ee8bf7bc8f21e4e8d68c76c
Andy Couto <andrea.couto@panerabread.com>
2020-06-21 11:34:44 -0400

    Transcribing from the mic, but theres an issue with setting the property in the model with the result

--- Voice/Model/SpeechToTextModel.swift
+++ Voice/Model/SpeechToTextModel.swift
@@ -8,18 +8,35 @@ class SpeechToTextModel: NSObject, ObservableObject
     @Published var buttonEnabled  = false
     let defaultTextForDisplay   = "🗣 ➡️ 📃️"
     // TODO: - published var for button image
-    
-    private var recording = false
+        
+    let speechRecognizer        = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+
+    var recognitionRequest      : SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask         : SFSpeechRecognitionTask?
+    let audioEngine             = AVAudioEngine()
 
     func toggleRecording()
     {
-        // TODO: - switch on whether recording is running, update button enabled & button image "mic"/"mic.fill"
-        recording.toggle()
-        print("recording: \(recording.description)")
+        // TODO: - button image "mic"/"mic.fill"
+
+        if audioEngine.isRunning
+        {
+            self.audioEngine.stop()
+            self.recognitionRequest?.endAudio()
+            self.buttonEnabled = false
+//            self.btnStart.setTitle("Start Recording", for: .normal)
+        }
+        else
+        {
+            self.startRecording()
+//            self.btnStart.setTitle("Stop Recording", for: .normal)
+        }
     }
     
     func setupSpeech()
     {
+        self.speechRecognizer?.delegate = self
+
         SFSpeechRecognizer.requestAuthorization
         {
             [weak self] (authStatus)
@@ -41,7 +58,80 @@ class SpeechToTextModel: NSObject, ObservableObject
     
     func startRecording()
     {
-        // TODO: -
+        if recognitionTask != nil
+        {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        do
+        {
+            try audioSession.setCategory(.record, mode: .measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        }
+        catch let error
+        {
+            print("There was an error setting up the recording session: \(error)")
+        }
+
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+
+        let inputNode = audioEngine.inputNode
+
+        guard let recognitionRequest = recognitionRequest else
+        {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+
+        recognitionRequest.shouldReportPartialResults = true
+
+        print("recording")
+        self.recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler:
+        {
+            (result, error)
+            in
+
+            if (error != nil || result?.isFinal ?? false), let result = result
+            {
+                // TODO: - textForDisplay not setting but the string is there in result??
+                // tried dispatching to main there's still an issue
+                self.textForDisplay = result.bestTranscription.formattedString
+                print("Text for display: \(String(describing: self.textForDisplay))")
+                print("result best transcription: \(result.bestTranscription.formattedString)")
+
+                if let error = error
+                {
+                    print("There was an error: \(error)")
+                }
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+
+                self.buttonEnabled = true
+            }
+        })
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat)
+        {
+            (buffer, when)
+            in
+            self.recognitionRequest?.append(buffer)
+        }
+
+        self.audioEngine.prepare()
+
+        do
+        {
+            try self.audioEngine.start()
+        }
+        catch
+        {
+            print("audioEngine couldn't start because of an error.")
+        }
     }
 }
 
